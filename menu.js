936fc65de77e624093eef4902d3b219585ec0534
Rakyail <enginozdemir190505@gmail.com>
2024-03-07 22:08:57 +0300

    Git LFS ile büyük dosyaları izleme başlatıldı

---
+++ menu.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.btn-link').on('click', function(e){
+      e.preventDefault();
+      var target = $($(this).attr('data-target'));
+      target.collapse('toggle'); 
+      target.on('shown.bs.collapse', function () {
+          var offset = target.offset().top - 180; // Hedefin pozisyonunu al ve 50 piksel aşağı kaydır
+          $('html, body').animate({
+              scrollTop: offset
+          }, 100);
+      });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var img = document.getElementById('fullscreen-img');
+
+    // Resmi ekranda tam boyutta göster
+    function showFullscreenImage() {
+        img.style.display = 'block';
+        setTimeout(function() {
+            img.style.display = 'none';
+        }, 5000); // 5 saniye sonra resmi gizle
+    }
+
+    showFullscreenImage(); // Sayfa yüklendiğinde resmi göster
+});
